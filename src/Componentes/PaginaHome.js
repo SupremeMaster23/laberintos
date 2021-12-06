dd62900f51e1a6f51b67b896307eb65497eda419
SupremeMaster23 <gabydiaz2000@unitec.edu>
2021-12-05 20:32:21 -0600

    5/12/21

--- src/Componentes/PaginaHome.js
+++ src/Componentes/PaginaHome.js
@@ -1,9 +1,25 @@
-import React, { Component } from "react";
-
+import React, { Component , useEffect,useState } from "react";
+import { matriz_metodo } from "./matriz_metodos";
 export default function PaginaHome() {
+
+  
+   function x(){
+     console.log("x");
+     return 5;
+   };
+   function y(){
+     debugger;
+     let meth = new matriz_metodo(0,0,0);
+     let g = meth.filas;
+     console.log(meth.columnas);
+     
+   }
   return (
     <div style={{marginLeft:'100px',marginRight:'100px',color:'Red'}}>
       <h1>Home</h1>
+      <button onClick={y}></button>
+      <input></input>
+      <input></input>
       </div>
   );
 }
---
+++ src/Componentes/matriz_metodos.js
@@ -0,0 +1,14 @@
+
+export class matriz_metodo {
+    
+    constructor(columnas , filas, k){
+      columnas=3;
+      filas=0;
+      k =0;
+    }
+
+    Pedir(f , c){
+      this.filas =f;
+      this.columnas=c;
+    };
+  }
